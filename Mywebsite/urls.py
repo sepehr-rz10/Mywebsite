45f6817415eb9a3c158e5fabfc3efec24b9a0693
Sepehr Rezaei <sepehrrezaei2006@gmail.com>
2025-10-08 19:24:57 +0330

    Created first app
    Created first app named website with three views for contact , about and index

--- Mywebsite/urls.py
+++ Mywebsite/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('' , include('website.urls'))
+     #path('Blog/' , include('blog.urls'))
+    
 ]
---
+++ website/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse , JsonResponse
+
+def index_view(request):
+    return HttpResponse('Home Page')
+def about_view(request):
+    return HttpResponse('<h1>About Page</h1>')
+def contact_view(request):
+    return JsonResponse({'name' : 'sepehr rezaei'})
+# Create your views here.
