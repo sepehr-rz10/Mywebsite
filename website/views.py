7697b718aed790585be4eaa69ecc857a73120641
Sepehr Rezaei <sepehrrezaei2006@gmail.com>
2025-10-08 23:59:47 +0330

    Created render html files
    Addes templates folder and html files to website app and render to view also added the name of the app to INSTALLED_APPS

--- website/views.py
+++ website/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse , JsonResponse
 
 def index_view(request):
-    return HttpResponse('Home Page')
+    return render(request , 'website/index.html')
 def about_view(request):
-    return HttpResponse('<h1>About Page</h1>')
+    return render(request , 'website/about.html')
 def contact_view(request):
-    return JsonResponse({'name' : 'sepehr rezaei'})
+    return render(request , 'website/contact.html')
+
+
+
+# def about_view(request):
+    # return HttpResponse('<h1>About Page</h1>')
+# def contact_view(request):
+    # return JsonResponse({'name' : 'sepehr rezaei'})
 # Create your views here.
