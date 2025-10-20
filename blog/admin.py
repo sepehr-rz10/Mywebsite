f2a891a8fa24323a266f739b359a35c93bd847fa
Sepehr Rezaei <sepehrrezaei2006@gmail.com>
2025-10-20 16:57:17 +0330

    Edited database and edited Admin panel
    + Created a simple model for blog app
    + Customized Admin panel
    + Created a model for contact in website app
    +

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from blog.models import Post
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    emty_value_display = '-emty-'
+    fields = ('title' ,)
+    list_display = ('title' , 'counted_views' , 'status' , 'published_date' , 'created_date')
+    list_filter = ('status' ,)
+    #ordering = ['-created_date']
+    search_fields = ['title' , 'content']
+
+#admin.site.register(Post , PostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-
 def blog_view(request):
     return render(request , 'blog/blog-home.html')
 def blog_single(request):
