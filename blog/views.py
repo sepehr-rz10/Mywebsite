1beb11009b18d7a2ef7c294ba21b8ceb945d140a
Sepehr Rezaei <sepehrrezaei2006@gmail.com>
2025-10-23 12:58:03 +0330

    Added some fields to database and completed the blog things and other things
    + Added author , image , category fields to database
    + Completed the structure of the blog
    + Truncate the contents
    + Used Include template tag
    + Showed published date of contents and categories
    + Completed the parameters of the urls of blog pages

--- blog/views.py
+++ blog/views.py
@@ -5,10 +5,14 @@ def blog_view(request):
     posts = Post.objects.filter(status = 1)
     context = {'posts' : posts}
     return render(request , 'blog/blog-home.html' , context)
+
 def blog_single(request , pid):
-    post = get_object_or_404(Post , pk = pid)
+    posts = Post.objects.filter(status = 1)
+    post = get_object_or_404(posts , pk = pid)
+    #post = get_object_or_404(Post , pk = pid , status = 1)
     context = {'post' :post}
     return render(request , 'blog/blog-single.html' , context)
+
 def test(request , pid):
     #post = Post.objects.get(id=pid)
     post = get_object_or_404(Post , pk = pid)
