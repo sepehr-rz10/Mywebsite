9202965a424ae615cfd8ceede0eb1a8d3de684c1
Sepehr Rezaei <sepehrrezaei2006@gmail.com>
2025-11-12 11:31:44 +0330

    Created a template tag file and filtered posts based on different items and added pagination
    + Created template tag file and some template tags
    + Filtered posts based on their authors , search form
    + Added pagination to the blog-home template

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render , get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator , EmptyPage , PageNotAnInteger
 
-def blog_view(request):
+def blog_view(request , **kwargs):
     posts = Post.objects.filter(status = 1)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username =kwargs['author_username'])
+    try:
+        posts = Paginator(posts , 2)
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
     context = {'posts' : posts}
     return render(request , 'blog/blog-home.html' , context)
 
@@ -13,11 +26,27 @@ def blog_single(request , pid):
     context = {'post' :post}
     return render(request , 'blog/blog-single.html' , context)
 
-def test(request , pid):
-    #post = Post.objects.get(id=pid)
-    post = get_object_or_404(Post , pk = pid)
-    context = {'post' :post}
-    return render(request , 'test.html' , context)
+def test(request):
+    return render(request , 'test.html')
+
+def blog_category(request , cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name)
+    context = {'posts' :posts}
+    return render(request , 'blog/blog-home.html' , context)
+
+def blog_search(request):
+    #print(request.__dict__)
+    posts = Post.objects.filter(status=1)
+    if request.method == 'GET':
+       #print(request.GET.get('s')) 
+       if s := request.GET.get('s'):
+            posts=posts.filter(content__contains=s)
+    context = {'posts' :posts}
+    return render(request , 'blog/blog-home.html' , context)
+
+
+
 
 #def test(request , name , family_name , age):
    # context = {'name' : name , 'family_name' : family_name , 'age' : age }
